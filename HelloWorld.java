5dd3b2e27916e3a0ef5b5f7ca26722d1988f9702
Siva <sankar.machani@gmail.com>
2021-04-04 17:37:37 +0530

    poll scm1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-                System.out.pritln("Hey Poll SCM !");
+                System.out.println("Hey Poll SCM !");
 	}
 }
 
